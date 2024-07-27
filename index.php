56f85e15b8864970b2534d24b668a4afb6d3113c
Bogdan Barbu <bogdanbarbuflorin@gmail.com>
2024-07-27 20:36:08 +0300

    imbunatatire majora portal profesor

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
             
             include_once("db-socket.php");
             
-            $result = QueryDB('SELECT * FROM `users` WHERE email="'.$_POST["email"].'" AND pass=MD5("'.$_POST["password"].'")');
+            $result = QueryDB('SELECT * FROM `users` WHERE email="'.$_POST["email"].'" AND pass=MD5("'.$_POST["password"].'") AND rol=2');
             var_dump($result);
             if($result->num_rows == 1) {
                 
@@ -63,7 +63,7 @@
             
             include_once("db-socket.php");
             
-            $result = QueryDB('SELECT * FROM `users` WHERE email="'.$_POST["email"].'" AND pass=MD5("'.$_POST["password"].'")');
+            $result = QueryDB('SELECT * FROM `users` WHERE email="'.$_POST["email"].'" AND pass=MD5("'.$_POST["password"].'") AND rol=1');
                    
             if($result->num_rows == 1) {
                 
@@ -71,6 +71,7 @@
                 $user = $result->fetch_assoc();
                 $_SESSION["user"]["type"] = "profesor";
                 $_SESSION["login_time"] = time();
+                $_SESSION["user"]["data"] = $user;
                 
                 header("Location: profesor/p_dashboard.php");
                 exit;
@@ -91,6 +92,7 @@
         <!-- Librarie design bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
+    
     <body>
         <!-- Butoane Modal login student/profesor -->
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalLoginStudent">LogIn Student</button>
@@ -133,7 +135,7 @@
                 <div class="modal-body">
                     <div class="row">
                         <div class="col">
-                            <img src="/assets/imagini/login.png" alt="imagineLogin">
+                            <img src="/assets/img/illustrations/login.png" alt="imagineLogin">
                         </div>
                         <div class="col">
                             <h2>Bine ai revenit!</h2>
@@ -141,11 +143,11 @@
                             <form action="/index.php" method="POST">
                                 <div class="mb-3">
                                     <label for="email" class="form-label">Email</label>
-                                    <input type="email" class="form-control" id="email" aria-describedby="email">
+                                    <input type="email" name="email" class="form-control" id="email" aria-describedby="email">
                                 </div>
                                 <div class="mb-3">
                                     <label for="parola" class="form-label">Parola</label>
-                                    <input type="password" class="form-control" id="parola">
+                                    <input type="password" name="password" class="form-control" id="parola">
                                 </div>
                                 <input type="hidden" name="type" value="login-profesor">
                                 <button type="submit" class="btn btn-primary">Conecteaza-te</button>
